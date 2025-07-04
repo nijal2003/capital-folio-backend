42395b1a440015e42323375c6f19d4be66df37a8
Nijal Ghodasara <nijalghodasara2003@gmail.com>
2025-07-04 09:26:03 +0530

    first commit

---
+++ src/main/java/com/example/capital_folio_backend/CapitalFolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.capital_folio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalFolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalFolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capital_folio_backend/CapitalFolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capital_folio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalFolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
