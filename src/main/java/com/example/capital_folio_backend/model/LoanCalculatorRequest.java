b5b97f09d38f05cfb2dff549a33bc16918af48c0
Nijal V. Ghodasara <100347724+nijal2003@users.noreply.github.com>
2025-07-05 00:34:06 +0530

    Update LoanCalculatorRequest.java

--- src/main/java/com/example/capital_folio_backend/model/LoanCalculatorRequest.java
+++ src/main/java/com/example/capital_folio_backend/model/LoanCalculatorRequest.java
@@ -4,28 +4,4 @@ public class LoanCalculatorRequest {
     private double principal;
     private double annualInterestRate;
     private int tenureYears;
-
-    public double getPrincipal() {
-        return principal;
-    }
-
-    public void setPrincipal(double principal) {
-        this.principal = principal;
-    }
-
-    public void setAnnualInterestRate(double annualInterestRate) {
-        this.annualInterestRate = annualInterestRate;
-    }
-
-    public void setTenureYears(int tenureYears) {
-        this.tenureYears = tenureYears;
-    }
-
-    public double getAnnualInterestRate() {
-        return annualInterestRate;
-    }
-
-    public int getTenureYears() {
-        return tenureYears;
-    }
 }
