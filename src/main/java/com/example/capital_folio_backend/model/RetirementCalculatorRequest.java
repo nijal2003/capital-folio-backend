f3350b7632a360ab6fb3d5ea62fdf7c3b00ca026
Nijal V. Ghodasara <100347724+nijal2003@users.noreply.github.com>
2025-07-05 00:37:42 +0530

    Update RetirementCalculatorRequest.java

--- src/main/java/com/example/capital_folio_backend/model/RetirementCalculatorRequest.java
+++ src/main/java/com/example/capital_folio_backend/model/RetirementCalculatorRequest.java
@@ -8,35 +8,5 @@ public class RetirementCalculatorRequest {
     private double monthlyExpense;
     private double inflationRate;
 
-    public void setCurrentAge(int currentAge) {
-        this.currentAge = currentAge;
-    }
-
-    public void setRetirementAge(int retirementAge) {
-        this.retirementAge = retirementAge;
-    }
-
-    public void setMonthlyExpense(double monthlyExpense) {
-        this.monthlyExpense = monthlyExpense;
-    }
-
-    public void setInflationRate(double inflationRate) {
-        this.inflationRate = inflationRate;
-    }
-
-    public int getCurrentAge() {
-        return currentAge;
-    }
-
-    public int getRetirementAge() {
-        return retirementAge;
-    }
-
-    public double getMonthlyExpense() {
-        return monthlyExpense;
-    }
-
-    public double getInflationRate() {
-        return inflationRate;
-    }
+    
 }
