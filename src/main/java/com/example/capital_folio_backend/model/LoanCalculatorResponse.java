c0b187dee7033cea394229298a54349575eac853
Nijal V. Ghodasara <100347724+nijal2003@users.noreply.github.com>
2025-07-05 00:37:13 +0530

    Update LoanCalculatorResponse.java

--- src/main/java/com/example/capital_folio_backend/model/LoanCalculatorResponse.java
+++ src/main/java/com/example/capital_folio_backend/model/LoanCalculatorResponse.java
@@ -4,15 +4,5 @@ package com.example.capital_folio_backend.model;
 public class LoanCalculatorResponse {
     private double emi;
 
-    public LoanCalculatorResponse(double emi) {
-        this.emi = emi;
-    }
 
-    public double getEmi() {
-        return emi;
-    }
-
-    public void setEmi(double emi) {
-        this.emi = emi;
-    }
 }
