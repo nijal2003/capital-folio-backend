79de7b925ce68312a15944f9a09ae1593bf99e43
Rushi Javiya <139797446+Rushi-Javiya@users.noreply.github.com>
2025-07-09 10:00:49 +0530

    Update RetirementCalculatorResponse.java

--- src/main/java/com/example/capital_folio_backend/dto/RetirementCalculatorResponse.java
+++ src/main/java/com/example/capital_folio_backend/dto/RetirementCalculatorResponse.java
@@ -1,16 +1,17 @@
 package com.example.capital_folio_backend.dto;
 
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
-@Validated
 @NoArgsConstructor
 @AllArgsConstructor
 public class RetirementCalculatorResponse {
-    private double estimatedCorpus;
 
+    @NotNull(message = "Estimated corpus must not be null")
+    @Min(value = 1, message = "Estimated corpus must be greater than 0")
+    private double estimatedCorpus;
 }
