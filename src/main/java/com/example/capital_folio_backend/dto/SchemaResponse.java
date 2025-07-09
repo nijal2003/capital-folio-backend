b47e1121a87b86e45b0a071b0943534878bf5d78
Rushi Javiya <139797446+Rushi-Javiya@users.noreply.github.com>
2025-07-09 10:01:03 +0530

    Update SchemaResponse.java

--- src/main/java/com/example/capital_folio_backend/dto/SchemaResponse.java
+++ src/main/java/com/example/capital_folio_backend/dto/SchemaResponse.java
@@ -1,22 +1,28 @@
 package com.example.capital_folio_backend.dto;
 
-
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Data
-@Validated
 @NoArgsConstructor
 @AllArgsConstructor
 public class SchemaResponse {
 
+    @NotBlank(message = "Type must not be blank")
     private String type;
 
+    @NotBlank(message = "Title must not be blank")
     private String title;
 
+    @NotNull(message = "Fields must not be null")
+    @NotEmpty(message = "Fields list must not be empty")
+    @Valid
     private List<FieldResponse> fields;
 }
