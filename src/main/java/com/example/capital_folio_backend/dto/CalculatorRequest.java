8ca2691bc9c612d4885ecfdee1ff0918c7862e19
Rushi Javiya <139797446+Rushi-Javiya@users.noreply.github.com>
2025-07-09 09:59:18 +0530

    Update CalculatorRequest.java

--- src/main/java/com/example/capital_folio_backend/dto/CalculatorRequest.java
+++ src/main/java/com/example/capital_folio_backend/dto/CalculatorRequest.java
@@ -1,17 +1,21 @@
 package com.example.capital_folio_backend.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Validated
 public class CalculatorRequest {
 
+    @NotBlank(message = "Type must not be blank")
     private String type;
 
+    @NotNull(message = "Inputs must not be null")
+    @Valid
     private LoanCalculatorRequest inputs;
 }
