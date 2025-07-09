c8056f64b1aa54b5560ca7d0c35c8c05c859a3af
Rushi Javiya <139797446+Rushi-Javiya@users.noreply.github.com>
2025-07-09 09:59:36 +0530

    Update FieldResponse.java

--- src/main/java/com/example/capital_folio_backend/dto/FieldResponse.java
+++ src/main/java/com/example/capital_folio_backend/dto/FieldResponse.java
@@ -1,23 +1,25 @@
 package com.example.capital_folio_backend.dto;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Validated
 public class FieldResponse {
 
+    @NotBlank(message = "Key must not be blank")
     private String key;
 
+    @NotBlank(message = "Label must not be blank")
     private String label;
 
+    @NotBlank(message = "Type must not be blank")
     private String type;
 
+    @NotNull(message = "Required field must not be null")
     private Boolean required;
-
 }
