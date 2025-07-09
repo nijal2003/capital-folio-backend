161b4b24bfb350b3fd50159ffe0b6de599addca5
Rushi Javiya <139797446+Rushi-Javiya@users.noreply.github.com>
2025-07-09 10:00:19 +0530

    Update LoanCalculatorResponse.java

--- src/main/java/com/example/capital_folio_backend/dto/LoanCalculatorResponse.java
+++ src/main/java/com/example/capital_folio_backend/dto/LoanCalculatorResponse.java
@@ -1,16 +1,17 @@
 package com.example.capital_folio_backend.dto;
 
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Validated
 public class LoanCalculatorResponse {
-    private double emi;
 
+    @NotNull(message = "EMI must not be null")
+    @Min(value = 1, message = "EMI must be greater than 0")
+    private double emi;
 }
